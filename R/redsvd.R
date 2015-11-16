a2c109a2fce72904f2f24ddaaf2243d214085a65
kafku <kazunoko93@gmail.com>
2015-11-17 03:11:04 +0900

    convert to dgCMatrix

--- R/redsvd.R
+++ R/redsvd.R
@@ -1,17 +1,26 @@
 redsvd <- function(A,num){
   if (is(A,"CsparseMatrix")) {
+    if (is(A, "symmetricMatrix")) {
+      A <- as(A, "dgCMatrix")
+    }
     redSVDwrap(A, num)
   }
 }
 
 redpca <- function(A,num){
   if (is(A,"CsparseMatrix")) {
+    if (is(A, "symmetricMatrix")) {
+      A <- as(A, "dgCMatrix")
+    }
     redPCAwrap(A, num)
   }
 }
 
 redSymEigen <- function(A,num){
   if (is(A,"CsparseMatrix")) {
+    if (is(A, "symmetricMatrix")) {
+      A <- as(A, "dgCMatrix")
+    }
     redSymwrap(A, num)
   }
 }
